039e3479b89808395ca067cfb8ee772a7d025c06
S4Lsalsoft <grupo.fisica.638@gmail.com>
2020-03-22 11:44:19 -0430

    no message

--- Halo Universal AimBot/My Project/Settings.Designer.vb
+++ Halo Universal AimBot/My Project/Settings.Designer.vb
@@ -116,7 +116,7 @@ Namespace My
         
         <Global.System.Configuration.UserScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("0")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("16")>  _
         Public Property s_aimKey() As Integer
             Get
                 Return CType(Me("s_aimKey"),Integer)
